77264a3da64f446525391d45483caa30c4763318
benniezhu <zhu.jieming.benjamin@gmail.com>
2020-09-27 23:42:16 -0400

    add the color scheme and the full/reduced to the server

--- server.R
+++ server.R
@@ -12,7 +12,9 @@ shinyServer(function(input,output){
            aes( x = location_name , y = Hospitalizations/1000 , fill = insurance))+
       geom_bar(stat = 'identity')+
       xlab('State') + ylab('Number of Hospitalizations')+labs(fill = 'Insurance')+
-      theme(axis.text.x = element_text(angle = 90))
+      theme(axis.text.x = element_text(angle = 90))+
+      scale_fill_manual(breaks = c("medicaid", "medicare" , "private" , "uninsured"),
+                        values = c("red4", "green4", "blue4" , "purple4"))
   })
   
   output$nonicuhosp_plot <- renderPlot({
@@ -20,7 +22,9 @@ shinyServer(function(input,output){
            aes( x = location_name , y = nonicuhosp/1000 , fill = insurance))+
       geom_bar(stat = 'identity')+
       xlab('State') + ylab('Number of Non ICU Hospitalizations')+labs(fill = 'Insurance')+
-      theme(axis.text.x = element_text(angle = 90))
+      theme(axis.text.x = element_text(angle = 90))+
+      scale_fill_manual(breaks = c("medicaid", "medicare" , "private" , "uninsured"),
+                        values = c("red4", "green4", "blue4" , "purple4"))
   })
   
 
@@ -29,31 +33,39 @@ shinyServer(function(input,output){
            aes(x = location_name, y = ICUs/1000, fill = insurance))+
       geom_bar(stat = 'identity')+
       xlab('State') + ylab('Number of ICU Admissions') + labs(fill = 'Insurance')+
-      theme(axis.text =  element_text(angle = 90))
+      theme(axis.text =  element_text(angle = 90))+
+      scale_fill_manual(breaks = c("medicaid", "medicare" , "private" , "uninsured"),
+                        values = c("red4", "green4", "blue4" , "purple4"))
   })  
   
   output$nonicuhosp_cost_plot <-renderPlot({
     ggplot(filter(Combined, location_name %in% input$State & IHME == input$IHME & Wave == input$Wave & inpatientcost_assumption == input$inpatientcost & uninsured_as == input$uninsuredinpatientcost), 
-           aes(x = location_name, y = (nonicucost)/1000000, fill = payer))+
+           aes_string(x = "location_name", y = if_else(input$uninsuredoop == 'Reduced' ,"nonicucost_reduced/1000000", "nonicucost/1000000"), fill = "payer"))+
       geom_bar(stat = 'identity')+
       xlab('State') + ylab('Cost of Non-ICU Admissions in $Millions') + labs(fill = 'Insurance')+
-      theme(axis.text =  element_text(angle = 90))
+      theme(axis.text =  element_text(angle = 90))+
+      scale_fill_manual(breaks = c("Medicaid Reimbursed", "Medicare Reimbursed" , "Private Reimbursed" , "Uninsured Reimbursed", "Medicaid OOP", "Medicare OOP", "Private OOP", "Uninsured OOP"),
+                        values = c("red4", "green4", "blue4" , "purple4", "red", "green", "blue" , "purple" ))
   })
   
   output$icu_cost_plot <-renderPlot({
     ggplot(filter(Combined, location_name %in% input$State & IHME == input$IHME & Wave == input$Wave & inpatientcost_assumption == input$inpatientcost & uninsured_as == input$uninsuredinpatientcost), 
-           aes(x = location_name, y = icucost/1000000, fill = payer))+
+           aes_string(x = "location_name", y = if_else(input$uninsuredoop == 'Reduced' , "icucost_reduced/1000000", "icucost/1000000"), fill = "payer"))+
       geom_bar(stat = 'identity')+
       xlab('State') + ylab('Cost of ICU Admissions in $Millions') + labs(fill = 'Insurance')+
-      theme(axis.text =  element_text(angle = 90))
+      theme(axis.text =  element_text(angle = 90))+
+      scale_fill_manual(breaks = c("Medicaid Reimbursed", "Medicare Reimbursed" , "Private Reimbursed" , "Uninsured Reimbursed", "Medicaid OOP", "Medicare OOP", "Private OOP", "Uninsured OOP"),
+                        values = c("red4", "green4", "blue4" , "purple4", "red", "green", "blue" , "purple" ))
   })
   
   output$hosp_cost_plot <-renderPlot({
     ggplot(filter(Combined, location_name %in% input$State & IHME == input$IHME & Wave == input$Wave & inpatientcost_assumption == input$inpatientcost & uninsured_as == input$uninsuredinpatientcost), 
-           aes(x = location_name, y = hospitalizationcost/1000000, fill = payer))+
+           aes_string(x = "location_name", y = if_else(input$uninsuredoop == 'Reduced' ,"hospitalizationcost_reduced/1000000", "hospitalizationcost/1000000" ), fill = "payer"))+
       geom_bar(stat = 'identity')+
       xlab('State') + ylab('Cost of Hospital Admissions Admissions in $Millions') + labs(fill = 'Insurance')+
-      theme(axis.text =  element_text(angle = 90))
+      theme(axis.text =  element_text(angle = 90))+
+      scale_fill_manual(breaks = c("Medicaid Reimbursed", "Medicare Reimbursed" , "Private Reimbursed" , "Uninsured Reimbursed", "Medicaid OOP", "Medicare OOP", "Private OOP", "Uninsured OOP"),
+                        values = c("red4", "green4", "blue4" , "purple4", "red", "green", "blue" , "purple" ))
   })
   
   
